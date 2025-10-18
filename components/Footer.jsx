5913f426048b987ede8621370400aa58d0c84df0
Adwira <aryadwiputra1284@gmail.com>
2025-10-18 15:18:18 +0700

    add organizational structure, staff carousel, and adjustment footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Footer({ site }){
   return (
-    <footer className="footer">
+    <footer className="footer" id="footer">
       <div className="card rounded-0 m-0">
         <div className="card-body px-0 pb-0 pt-1 border-top">
           <div className="container p-4">
@@ -17,9 +17,19 @@ export default function Footer({ site }){
                   <span>Kecamatan Capkala, Kabupaten Bengkayang,</span>
                   <span>Provinsi Kalimantan Barat, Kode Pos 79211</span>
                 </div>
+              </div>
 
+              <div className="col-lg-6 d-flex justify-content-lg-center">
                 <div className="d-flex flex-column gap-2 mb-4">
                   <a className="link-footer d-flex gap-3 align-items-center" href="mailto:example@gmail.com">
+                    <span className="icon"><i className="bi bi-instagram"></i></span>
+                    <span>Instagram</span>
+                  </a>
+                  <a className="link-footer d-flex gap-3 align-items-center" href="https://wa.me/+6281236626752">
+                    <span className="icon"><i className="bi bi-facebook"></i></span>
+                    <span>Facebook</span>
+                  </a>
+                  <a className="link-footer d-flex gap-3 align-items-center" href="https://wa.me/+6281236626752">
                     <span className="icon"><i className="bi bi-envelope"></i></span>
                     <span>example@gmail.com</span>
                   </a>
@@ -28,58 +38,7 @@ export default function Footer({ site }){
                     <span>+6281236626752</span>
                   </a>
                 </div>
-              </div>
-
-              <div className="col-lg-6">
-                <div className="row mb-4">
-                  <div className="col">
-                    <h5 className="text-white mb-2">Menu</h5>
-
-                    <div className="d-flex flex-column gap-2">
-                      <Link href="/" className="link-footer">Beranda</Link>
-                      <div className="dropdown">
-                        <a className="link-footer dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Profil
-                        </a>
-                        <ul className="dropdown-menu">
-                          <li><Link className="dropdown-item" href="/profil/tentang">Tentang</Link></li>
-                          <li><Link className="dropdown-item" href="/profil/visi-misi">Visi & Misi</Link></li>
-                          <li><Link className="dropdown-item" href="/sotk">SOTK</Link></li>
-                          <li><Link className="dropdown-item" href="/data-penduduk">Data Penduduk</Link></li>
-                          <li><Link className="dropdown-item" href="/data-kewilayahan">Data Kewilayahan</Link></li>
-                        </ul>
-                      </div>
-                      <div className="dropdown">
-                        <a className="link-footer dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Informasi
-                        </a>
-                        <ul className="dropdown-menu">
-                          <li><Link className="dropdown-item" href="/berita">Berita</Link></li>
-                          <li><Link className="dropdown-item" href="/pengumuman">Pengumuman</Link></li>
-                          <li><Link className="dropdown-item" href="/layanan">Layanan</Link></li>
-                          <li><Link className="dropdown-item" href="/sarana-prasarana">Sarana & Prasarana</Link></li>
-                          <li><Link className="dropdown-item" href="/dokumen">Dokumen Publik</Link></li>
-                        </ul>
-                      </div>
-                      <Link href="/pariwisata" className="link-footer">Pariwisata</Link>
-                      <Link href="/umkm" className="link-footer">UMKM</Link>
-                      <Link href="/kontak" className="link-footer">Kontak</Link>
-                    </div>
-                  </div>
-
-                  <div className="col">
-                    <h5 className="text-white mb-2">Ikuti kami</h5>
-
-                    <div className="d-flex flex-wrap gap-2">
-                      <a href="#" target="_blank" className="btn btn-sm btn-footer icon"><i className="bi bi-instagram"></i></a>
-                      <a href="#" target="_blank" className="btn btn-sm btn-footer icon"><i className="bi bi-facebook"></i></a>
-                      <a href="#" target="_blank" className="btn btn-sm btn-footer icon"><i className="bi bi-twitter-x"></i></a>
-                      <a href="#" target="_blank" className="btn btn-sm btn-footer icon"><i className="bi bi-tiktok"></i></a>
-                      <a href="#" target="_blank" className="btn btn-sm btn-footer icon"><i className="bi bi-youtube"></i></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              </div> 
 
               <div className="col-lg-12">
                 <div className="row justify-content-center align-items-center g-2 border-top pt-3 mt-2">
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,26 +1,74 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 export default function Navbar(){
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const pathname = usePathname()
+  const router = useRouter()
 
-  const isActive = (path) => {
-    if (path === '/' && pathname === '/') return 'active'
-    if (path !== '/' && pathname.startsWith(path)) return 'active'
-    return ''
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  useEffect(() => {
+    setIsMenuOpen(false)
+  }, [pathname])
+
+  const handleSectionNavigation = (sectionId) => {
+    setIsMenuOpen(false)
+    
+    if (pathname === '/') {
+      setTimeout(() => {
+        const element = document.getElementById(sectionId)
+        if (element) {
+          const offset = 100
+          const elementPosition = element.getBoundingClientRect().top
+          const offsetPosition = elementPosition + window.pageYOffset - offset
+
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+          })
+        }
+      }, 100)
+    } else {
+      router.push(`/#${sectionId}`)
+    }
+  }
+
+  const handleFooterNavigation = () => {
+    setIsMenuOpen(false)
+    
+    if (pathname === '/') {
+      setTimeout(() => {
+        const footer = document.getElementById('footer')
+        if (footer) {
+          const offset = 100
+          const footerPosition = footer.getBoundingClientRect().top
+          const offsetPosition = footerPosition + window.pageYOffset - offset
+
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+          })
+        }
+      }, 100)
+    } else {
+      router.push('/#footer')
+    }
   }
 
   return (
     <header className="fixed-top">
       <div className="container p-4">
-        <div className="card rounded-4 border-0 m-0" id="navbar">
+        <div className="card rounded-4 border-0 m-0 shadow-sm">
           <div className="card-body p-0">
             <nav className="navbar navbar-expand-lg p-2">
               <div className="container px-2">
-                <Link className="navbar-brand text-uppercase" href="/">
+                {/* Brand/Logo */}
+                <Link className="navbar-brand text-uppercase" href="/" onClick={() => setIsMenuOpen(false)}>
                   <div className="d-flex justify-content-start align-items-center gap-2">
                     <svg width="40" height="40">
                       <image xlinkHref="/img/logo/logo.svg" width="40" height="40" />
@@ -31,64 +79,74 @@ export default function Navbar(){
                     </div>
                   </div>
                 </Link>
+
+                {/* Mobile Toggle Button */}
                 <button 
                   className="navbar-toggler border-0" 
                   type="button" 
-                  data-bs-toggle="collapse" 
-                  data-bs-target="#navbarNav" 
                   aria-controls="navbarNav" 
-                  aria-expanded="false" 
+                  aria-expanded={isMenuOpen}
                   aria-label="Toggle navigation"
-                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                  onClick={toggleMenu}
                 >
                   <span className="navbar-toggler-icon"></span>
                 </button>
+
+                {/* Navigation Menu */}
                 <div className={`collapse navbar-collapse py-2 ${isMenuOpen ? 'show' : ''}`} id="navbarNav">
-                  <ul className="navbar-nav ms-auto text-center">
-                    <li className="nav-item">
-                      <Link className={`nav-link ${isActive('/')}`} href="/">Beranda</Link>
+                  <ul className="navbar-nav ms-auto text-center align-items-center">
+                    <li className="nav-item mx-1">
+                      <Link 
+                        className={`nav-link ${pathname === '/' ? 'active' : ''}`} 
+                        href="/"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        Beranda
+                      </Link>
                     </li>
-                    <li className="nav-item dropdown">
-                      <a className={`nav-link dropdown-toggle ${isActive('/profil') || isActive('/sotk') || isActive('/data-penduduk') || isActive('/data-kewilayahan')}`} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Profil
-                      </a>
-                      <ul className="dropdown-menu px-2">
-                        <li><Link className={`dropdown-item ${isActive('/profil/tentang')}`} href="/profil/tentang">Tentang</Link></li>
-                        <li><Link className={`dropdown-item ${isActive('/profil/visi-misi')}`} href="/profil/visi-misi">Visi & Misi</Link></li>
-                        <li><Link className={`dropdown-item ${isActive('/sotk')}`} href="/sotk">SOTK</Link></li>
-                        <li><Link className={`dropdown-item ${isActive('/data-penduduk')}`} href="/data-penduduk">Data Penduduk</Link></li>
-                        <li><Link className={`dropdown-item ${isActive('/data-kewilayahan')}`} href="/data-kewilayahan">Data Kewilayahan</Link></li>
-                      </ul>
+                    <li className="nav-item mx-1">
+                      <button 
+                        className="nav-link border-0 bg-transparent p-2" 
+                        onClick={() => handleSectionNavigation('visi-misi')}
+                      >
+                        Tentang
+                      </button>
                     </li>
-                    <li className="nav-item dropdown">
-                      <a className={`nav-link dropdown-toggle ${isActive('/berita') || isActive('/pengumuman') || isActive('/layanan') || isActive('/sarana-prasarana') || isActive('/dokumen')}`} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Informasi
-                      </a>
-                      <ul className="dropdown-menu px-2">
-                        <li><Link className={`dropdown-item ${isActive('/berita')}`} href="/berita">Berita</Link></li>
-                        <li><Link className={`dropdown-item ${isActive('/pengumuman')}`} href="/pengumuman">Pengumuman</Link></li>
-                        <li><Link className={`dropdown-item ${isActive('/layanan')}`} href="/layanan">Layanan</Link></li>
-                        <li><Link className={`dropdown-item ${isActive('/sarana-prasarana')}`} href="/sarana-prasarana">Sarana & Prasarana</Link></li>
-                        <li><Link className={`dropdown-item ${isActive('/dokumen')}`} href="/dokumen">Dokumen Publik</Link></li>
-                      </ul>
+                    <li className="nav-item mx-1">
+                      <button 
+                        className="nav-link border-0 bg-transparent p-2" 
+                        onClick={() => handleSectionNavigation('struktur-organisasi')}
+                      >
+                        Struktur Organisasi
+                      </button>
                     </li>
-                    <li className="nav-item">
-                      <Link className={`nav-link ${isActive('/pariwisata')}`} href="/pariwisata">Pariwisata</Link>
+                    <li className="nav-item mx-1">
+                      <button 
+                        className="nav-link border-0 bg-transparent p-2" 
+                        onClick={() => handleSectionNavigation('data-wilayah')}
+                      >
+                        Data Wilayah
+                      </button>
                     </li>
-                    <li className="nav-item">
-                      <Link className={`nav-link ${isActive('/umkm')}`} href="/umkm">UMKM</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className={`nav-link ${isActive('/kontak')}`} href="/kontak">Kontak</Link>
+                    <li className="nav-item mx-1">
+                      <button 
+                        className="nav-link border-0 bg-transparent p-2" 
+                        onClick={handleFooterNavigation}
+                      >
+                        Kontak
+                      </button>
                     </li>
                   </ul>
 
-                  <div className="vr ms-2 me-3 d-none d-lg-inline"></div>
+                  {/* Separator */}
+                  <div className="vr ms-3 me-3 d-none d-lg-block"></div>
 
+                  {/* Theme Toggle */}
                   <ul className="navbar-nav">
                     <li className="nav-item">
-                      <div className="theme-toggle d-flex justify-content-center align-items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" className="iconify iconify--system-uicons" width="20" height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 21 21">
+                      <div className="theme-toggle d-flex justify-content-center align-items-center gap-2 px-3">
+                        {/* Light Mode Icon */}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 21 21" className="text-muted">
                           <g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
                             <path d="M10.5 14.5c2.219 0 4-1.763 4-3.982a4.003 4.003 0 0 0-4-4.018c-2.219 0-4 1.781-4 4c0 2.219 1.781 4 4 4zM4.136 4.136L5.55 5.55m9.9 9.9l1.414 1.414M1.5 10.5h2m14 0h2M4.135 16.863L5.55 15.45m9.899-9.9l1.414-1.415M10.5 19.5v-2m0-14v-2" opacity=".3"></path>
                             <g transform="translate(-210 -1)">
@@ -98,11 +156,14 @@ export default function Navbar(){
                             </g>
                           </g>
                         </svg>
-                        <div className="form-check form-switch fs-6 mt-1 ms-1">
-                          <input className="form-check-input me-0" type="checkbox" id="toggle-dark" style={{cursor: 'pointer'}} />
-                          <label className="form-check-label"></label>
+                        
+                        {/* Toggle Switch */}
+                        <div className="form-check form-switch fs-6 m-0">
+                          <input className="form-check-input" type="checkbox" id="toggle-dark" />
                         </div>
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" className="iconify iconify--mdi" width="20" height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                        
+                        {/* Dark Mode Icon */}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" className="text-muted">
                           <path fill="currentColor" d="m17.75 4.09l-2.53 1.94l.91 3.06l-2.63-1.81l-2.63 1.81l.91-3.06l-2.53-1.94L12.44 4l1.06-3l1.06 3l3.19.09m3.5 6.91l-1.64 1.25l.59 1.98l-1.7-1.17l-1.7 1.17l.59-1.98L15.75 11l2.06-.05L18.5 9l.69 1.95l2.06.05m-2.28 4.95c.83-.08 1.72 1.1 1.19 1.85c-.32.45-.66.87-1.08 1.27C15.17 23 8.84 23 4.94 19.07c-3.91-3.9-3.91-10.24 0-14.14c.4-.4.82-.76 1.27-1.08c.75-.53 1.93.36 1.85 1.19c-.27 2.86.69 5.83 2.89 8.02a9.96 9.96 0 0 0 8.02 2.89m-1.64 2.02a12.08 12.08 0 0 1-7.8-3.47c-2.17-2.19-3.33-5-3.49-7.82c-2.81 3.14-2.7 7.96.31 10.98c3.02 3.01 7.84 3.12 10.98.31Z"></path>
                         </svg>
                       </div>
